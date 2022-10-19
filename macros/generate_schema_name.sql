710b581654bdd3667b742ecd1e535d5ee384426e
cprdanonenestle@gmail.com <cprdanonenestle@gmail.com>
2022-10-19 22:41:54 +0000

    Macro OK

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,17 +1,25 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is not none and 'DBT_PRO' in target.name -%}
+    {%- if custom_schema_name is not none and target.name == 'DEVELOPER' -%}
 
-        {{ custom_schema_name}}
+        {{ default_schema }}_{{ custom_schema_name | trim }}
 
-    {%- elif custom_schema_name is not none and 'DBT_DEV' in target.name -%}
-    
-        {{ custom_schema_name}}
+    {%- elif custom_schema_name is not none and target.name == 'PRO' -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- elif custom_schema_name is not none and target.name == 'DEV' -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- elif custom_schema_name is none -%}
+
+        {{ default_schema }}
 
     {%- else -%}
 
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ default_schema }}_ERROR_{{ custom_schema_name | trim }}
 
     {%- endif -%}
 
