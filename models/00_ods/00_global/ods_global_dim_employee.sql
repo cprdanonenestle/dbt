6bf7ea0a0afa7ba0ac68464d1bc3494f89a91955
cprdanonenestle@gmail.com <cprdanonenestle@gmail.com>
2022-10-19 13:01:07 +0000

    f5

--- models/00_ods/00_global/ods_global_dim_employee.sql
+++ models/00_ods/00_global/ods_global_dim_employee.sql
@@ -3,6 +3,23 @@ with
 ---#########  SOURCES  #########
 DIM_EMPLOYEE_STG as(
     select * from {{ source('STG', 'DIM_EMPLOYEE') }}
+),
+
+CSV_DIM_POSITIONS as(
+    select * from {{ ref('CSV_DIM_POSITIONS')}}
+),
+
+---#########  TRANSFORM  #########
+JOIN_POSITION as(
+    select
+        empl.ID_EMPLYEE AS ID_EMPLOYEE
+        ,empl.NAME
+        ,empl.ID_DEPARTMENT
+        ,pos.ID_POSITION
+        ,pos.POSITION
+    from DIM_EMPLOYEE_STG empl
+    left join CSV_DIM_POSITIONS pos
+    on empl.ID_POSITION = pos.ID_POSITION
 )
 
-select * from DIM_EMPLOYEE_STG
+select * from JOIN_POSITION
